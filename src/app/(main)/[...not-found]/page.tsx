d16c605634d5f68443d1554ae8b3003e89078145
felixesteban100 <felixesteban100@gmail.com>
2026-09-12 12:13:24 -0400

    Don't save cache in random page

--- src/app/(main)/[...not-found]/page.tsx
+++ src/app/(main)/[...not-found]/page.tsx
@@ -1,10 +1,5 @@
 import { BrushCleaning } from "lucide-react";
 
-// TODO: Cache Components adoption. Refactor this route so this opt-out can be removed.
-// See: https://nextjs.org/docs/app/guides/migrating-to-cache-components
-
-export const instant = false
-
 export default function NotFound() {
     return (
         <div className="min-h-[70vh] flex flex-col items-center justify-center">
